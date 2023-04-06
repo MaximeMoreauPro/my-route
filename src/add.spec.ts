bf438aef88dd25500aca792d93024ee866153c0a
MaximeMoreauPro <117015980+MaximeMoreauPro@users.noreply.github.com>
2023-04-06 12:24:57 +0100

    test: add stryker mutation testing tool

--- src/add.spec.ts
+++ src/add.spec.ts
@@ -2,7 +2,16 @@ import { add } from "./add"
 
 describe('add', () => {
     it('should return the sum of the 2 parameters', () => {
-        const result = add(1,2);
-        expect(result).toBe(3)
+        const result = add(0,0);
+        expect(result).toBe(0);
+
+        const result2 = add(-5,5);
+        expect(result2).toBe(0);
+
+        const result3 = add(-5,-5);
+        expect(result3).toBe(-10);
+
+        const result4 = add(5,5);
+        expect(result4).toBe(10)
     })
 })

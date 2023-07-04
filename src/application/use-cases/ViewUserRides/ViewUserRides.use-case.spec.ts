e48978607d747c561fd133b7dfb09e8a530b46a6
MaximeMoreauPro <117015980+MaximeMoreauPro@users.noreply.github.com>
2023-07-04 21:19:15 +0100

    test: use ride builder

--- src/application/use-cases/ViewUserRides/ViewUserRides.use-case.spec.ts
+++ src/application/use-cases/ViewUserRides/ViewUserRides.use-case.spec.ts
@@ -3,7 +3,8 @@ import {
   Fixture,
   createFixture,
 } from './ViewUserRides.use-case.test-fixture';
-import { Alex, Zoe } from '../../../infrastructure/tests/User.test-data';
+import { Alex, Bob, Zoe } from '../../../infrastructure/tests/User.test-data';
+import { rideBuilder } from '../../../infrastructure/tests/Ride.builder';
 
 describe('Feature: view user rides', () => {
   let fixture: Fixture;
@@ -14,138 +15,35 @@ describe('Feature: view user rides', () => {
 
   describe('Rule: the rides are displayed by departure date and time in chronological order', () => {
     test("A user can view the 3 Alex's rides", async () => {
-      fixture.givenTheseRidesExist([
-        {
-          id: '1',
-          driver: Alex,
-          departurePlace: 'London',
-          departureTime: '2023-01-01T12:30:00.000Z',
-          destinationPlace: 'Brighton',
-          destinationTime: '2023-01-01T14:30:00.000Z',
-          postedAt: '2023-01-01T08:30:00.000Z',
-          passengers: [],
-        },
-        {
-          id: '2',
-          driver: Zoe,
-          departurePlace: 'Manchester',
-          departureTime: '2023-01-01T10:30:00.000Z',
-          destinationPlace: 'Liverpool',
-          destinationTime: '2023-01-01T12:30:00.000Z',
-          postedAt: '2023-01-01T09:30:00.000Z',
-          passengers: [],
-        },
-        {
-          id: '3',
-          driver: Alex,
-          departurePlace: 'Brighton',
-          departureTime: '2022-12-01T12:30:00.000Z',
-          destinationPlace: 'London',
-          destinationTime: '2022-12-01T14:30:00.000Z',
-          postedAt: '2022-12-01T08:30:00.000Z',
-          passengers: [],
-        },
-        {
-          id: '4',
-          driver: Alex,
-          departurePlace: 'London',
-          departureTime: '2023-02-01T12:30:00.000Z',
-          destinationPlace: 'Liverpool',
-          destinationTime: '2023-02-01T18:30:00.000Z',
-          postedAt: '2022-02-01T08:30:00.000Z',
-          passengers: [],
-        },
-      ]);
+      const alexRide1 = rideBuilder().drivenBy(Alex).build();
+      const alexRide2 = rideBuilder().drivenBy(Alex).build();
+      const zoeRide = rideBuilder().drivenBy(Zoe).build();
+      const bobRide = rideBuilder().drivenBy(Bob).build();
+
+      fixture.givenTheseRidesExist([alexRide1, zoeRide, alexRide2, bobRide]);
 
       await fixture.whenViewUserRides({
         user: Alex,
       });
 
-      fixture.thenDisplayedRidesShouldBe([
-        {
-          id: '3',
-          driver: Alex,
-          departurePlace: 'Brighton',
-          departureTime: '2022-12-01T12:30:00.000Z',
-          destinationPlace: 'London',
-          destinationTime: '2022-12-01T14:30:00.000Z',
-          postedAt: '2022-12-01T08:30:00.000Z',
-          passengers: [],
-        },
-        {
-          id: '1',
-          driver: Alex,
-          departurePlace: 'London',
-          departureTime: '2023-01-01T12:30:00.000Z',
-          destinationPlace: 'Brighton',
-          destinationTime: '2023-01-01T14:30:00.000Z',
-          postedAt: '2023-01-01T08:30:00.000Z',
-          passengers: [],
-        },
-        {
-          id: '4',
-          driver: Alex,
-          departurePlace: 'London',
-          departureTime: '2023-02-01T12:30:00.000Z',
-          destinationPlace: 'Liverpool',
-          destinationTime: '2023-02-01T18:30:00.000Z',
-          postedAt: '2022-02-01T08:30:00.000Z',
-          passengers: [],
-        },
-      ]);
+      fixture.thenDisplayedRidesShouldBe([alexRide1, alexRide2]);
     });
   });
 
   describe('Rule: the user is informed if a user has no ride', () => {
     test('Alex can view the message "Zoe Davies has no ride"', async () => {
-      fixture.givenTheseRidesExist([
-        {
-          id: '1',
-          driver: Alex,
-          departurePlace: 'London',
-          departureTime: '2023-01-01T12:30:00.000Z',
-          destinationPlace: 'Brighton',
-          destinationTime: '2023-01-01T14:30:00.000Z',
-          postedAt: '2023-01-01T08:30:00.000Z',
-          passengers: [],
-        },
-        {
-          id: '2',
-          driver: Alex,
-          departurePlace: 'Manchester',
-          departureTime: '2023-01-01T10:30:00.000Z',
-          destinationPlace: 'Liverpool',
-          destinationTime: '2023-01-01T12:30:00.000Z',
-          postedAt: '2023-01-01T09:30:00.000Z',
-          passengers: [],
-        },
-        {
-          id: '3',
-          driver: Alex,
-          departurePlace: 'Brighton',
-          departureTime: '2022-12-01T12:30:00.000Z',
-          destinationPlace: 'London',
-          destinationTime: '2022-12-01T14:30:00.000Z',
-          postedAt: '2022-12-01T08:30:00.000Z',
-          passengers: [],
-        },
-        {
-          id: '4',
-          driver: Alex,
-          departurePlace: 'London',
-          departureTime: '2023-02-01T12:30:00.000Z',
-          destinationPlace: 'Liverpool',
-          destinationTime: '2023-02-01T18:30:00.000Z',
-          postedAt: '2022-02-01T08:30:00.000Z',
-          passengers: [],
-        },
-      ]);
+      const alexRide = rideBuilder().drivenBy(Alex).build();
+      const bobRide = rideBuilder().drivenBy(Bob).build();
+
+      fixture.givenTheseRidesExist([alexRide, bobRide]);
 
       await fixture.whenViewUserRides({
         user: Zoe,
       });
 
-      fixture.thenDisplayedMessageShouldBe('Zoe Davies has no ride');
+      fixture.thenDisplayedMessageShouldBe(
+        `${Zoe.firstName} ${Zoe.lastName} has no ride`
+      );
     });
   });
 
@@ -155,25 +53,16 @@ describe('Feature: view user rides', () => {
         rideRepository: new ErrorInMemoryRideRepository(),
       });
 
-      fixture.givenTheseRidesExist([
-        {
-          id: '1',
-          driver: Zoe,
-          departurePlace: 'London',
-          departureTime: '2023-01-01T12:30:00.000Z',
-          destinationPlace: 'Brighton',
-          destinationTime: '2023-01-01T14:30:00.000Z',
-          postedAt: '2023-01-01T08:30:00.000Z',
-          passengers: [],
-        },
-      ]);
+      const zoeRide = rideBuilder().drivenBy(Zoe).build();
+
+      fixture.givenTheseRidesExist([zoeRide]);
 
       await fixture.whenViewUserRides({
         user: Zoe,
       });
 
       fixture.thenDisplayedMessageShouldBe(
-        "Zoe Davies's rides cannot be fetched. Please try later"
+        `${Zoe.firstName} ${Zoe.lastName}'s rides cannot be fetched. Please try later`
       );
     });
   });

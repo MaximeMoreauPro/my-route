f8e5bfb4fcbd297f6dca7156b08c83a86f76ae14
MaximeMoreauPro <117015980+MaximeMoreauPro@users.noreply.github.com>
2023-11-29 18:48:42 +0000

    fix: make tests works since workspace

--- jest.config.ts
+++ jest.config.ts
@@ -173,7 +173,7 @@ const config: Config = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['/node_modules/', 'dist/'],
+  testPathIgnorePatterns: ['/node_modules/', 'dist/', '.stryker-tmp/'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],

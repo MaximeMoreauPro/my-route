dbb24172f6023b5fe71418795b7940d64b595b97
MaximeMoreauPro <117015980+MaximeMoreauPro@users.noreply.github.com>
2023-11-29 15:44:11 +0000

    Add shared package in workspace (#84)

--- src/application/repositories/UserRepository.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '@/domain/User';
-
-export interface UserRepository {
-  save: (user: User) => Promise<void>;
-  getUser(userId: string): Promise<User | undefined>;
-  getUserByEmail(userEmail: string): Promise<User | undefined>;
-}
